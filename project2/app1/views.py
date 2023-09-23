2ead4325d1508a243de97f1e478a81e55db09724
msuleman2001 <msuleman2001@gmail.com>
2023-09-23 16:52:17 +0500

    Update views.py
    change by github

--- project2/app1/views.py
+++ project2/app1/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 #default and clear in vscode
 #changed by devops
+#change by github
 def fun1(request):
     a = "abc"
     b = "xyz"
