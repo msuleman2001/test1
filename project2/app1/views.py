cbcfde96704b9211e7b9fbf22f954ca68e21b6a2
msuleman2001 <msuleman2001@gmail.com>
2023-09-23 15:23:13 +0500

    changes are az url

--- project2/app1/views.py
+++ project2/app1/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # change in vs code
 #change on git
+#change after add az url
 # Create your views here.
 def fun1(request):
     a = "abc"
