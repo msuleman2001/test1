4a9e6fd2eaeefc61ebf4c198afc062953cd23405
msuleman2001 <msuleman2001@gmail.com>
2023-09-23 14:36:41 +0500

    test update

--- project2/app1/views.py
+++ project2/app1/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def fun1(request):
-    return HttpResponse("<h1>This is project 1 -> app1 -> fun1</h1>")
+    a = "abc"
+    b = "xyz"
+
+    return HttpResponse(a + b)
