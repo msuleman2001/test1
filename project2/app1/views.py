584a9f486f08ab7fd06a1f69d498c33661d30b99
msuleman2001 <msuleman2001@gmail.com>
2023-09-23 16:50:00 +0500

    change by vscode

--- project2/app1/views.py
+++ project2/app1/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# change in vs code
-#change on git
-#change after add az url
-# Create your views here.
-#change after new project test1 in devops
-#more changes added to repo
-#change by github
-#change by devops
+#default and clear in vscode
 def fun1(request):
     a = "abc"
     b = "xyz"
